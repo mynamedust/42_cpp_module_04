8e393489d3e59cb235a72311610cb1ed31ea06ab
mynamedust <mynamedust@gmail.com>
2023-10-04 17:30:45 +0400

    add ex02

---
+++ ex02/Cat.cpp
@@ -0,0 +1,33 @@
+#include "Cat.hpp"
+
+Cat::Cat() : Animal("Cat") {
+	std::cout << "Cat constructor called" << std::endl;
+	brain = new Brain();
+};
+
+Cat::~Cat() {
+	std::cout << "Cat destructor called" << std::endl;
+	delete brain;
+};
+
+Cat::Cat(const Cat &other) : Animal(other) {
+	std::cout << "Cat copy constructor called" << std::endl;
+	brain = new Brain();
+	*this = other;
+};
+
+Cat& Cat::operator=(const Cat &other) {
+	if (this != &other) {
+		this->_type = other._type;
+		*(this->brain) = *(other.brain);
+	}
+	return *this;
+};
+
+void Cat::makeSound() const{
+	std::cout << _type << ": Meow" << std::endl;
+}
+
+Brain* Cat::getBrain() const {
+	return brain;
+}
