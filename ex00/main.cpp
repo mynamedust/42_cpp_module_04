24d4e78861f3a89e439c876b3e8e5f671166b0c8
mynamedust <mynamedust@gmail.com>
2023-10-18 16:36:37 +0400

    fix ex00 Makefile

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -8,17 +8,21 @@ int main()
 {
 	std::cout << "----------Animals test----------" << std::endl;
 	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound();
-	j->makeSound();
+	const Animal* dog = new Dog();
+	const Animal* cat = new Cat();
+	Cat cat1;
+	Cat cat2(cat1);
+
+
+	std::cout << dog->getType() << " " << std::endl;
+	std::cout << cat->getType() << " " << std::endl;
+	cat->makeSound();
+	dog->makeSound();
 	meta->makeSound();
 
 	delete meta;
-	delete j;
-	delete i;
+	delete dog;
+	delete cat;
 	std::cout << "----------Wrong animals test----------" << std::endl;
 	const WrongAnimal* w = new WrongAnimal();
 	const WrongAnimal* wc = new WrongCat();
